7499d6273737421ad56b4e5d5c73291014f0f910
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-19 18:56:11 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,148 @@
+export const item = {
+    "id": "1f6e0593-369a-4529-aaa6-261f2e372453",
+    "practice_title": "stage",
+    "duration": "charge",
+    "scenario": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png",
+    "club_list": [
+        {
+            "id": "df091589-9c6e-42a9-90fa-b0e5281ef7e7",
+            "club_title": "claim",
+            "club_duration": "art",
+            "club_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png",
+            "image_list": [
+                {
+                    "id": "138fddbb-59d2-42ef-b41d-72ec271d7bb9",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png"
+                },
+                {
+                    "id": "9ed3ea1b-2db3-4ac9-af2f-55eab49f59f2",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/49.png"
+                },
+                {
+                    "id": "c7409119-5183-4cac-82cd-a7b98a028ad6",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "042df195-6dde-4026-b160-e67f5dc8451e",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/146.png"
+                },
+                {
+                    "id": "3cfd8825-e95b-4fb9-a491-877a7f2d7b91",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png"
+                }
+            ]
+        },
+        {
+            "id": "730a97e0-b662-4d34-9038-d8c31c45849c",
+            "club_title": "child",
+            "club_duration": "national",
+            "club_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/353.png",
+            "image_list": [
+                {
+                    "id": "4508eeed-ecba-4b35-afb2-d0804fb01cf6",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/80.png"
+                },
+                {
+                    "id": "6d5248fc-328b-4934-bbee-4b11a94646f9",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                },
+                {
+                    "id": "d69199ba-31a1-4954-9b83-7ac704a2df95",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/296.png"
+                },
+                {
+                    "id": "6ad1030c-2c0f-4207-b8ed-6da142d253db",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png"
+                },
+                {
+                    "id": "36213b4f-3cf4-460d-a796-2aebb3bdc256",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                }
+            ]
+        },
+        {
+            "id": "e9c893a0-b945-4531-bec0-1a01b4070180",
+            "club_title": "young",
+            "club_duration": "same",
+            "club_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/53.png",
+            "image_list": [
+                {
+                    "id": "1cb70c1c-8b8d-43be-8232-fb1a1b5bb1ca",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                },
+                {
+                    "id": "f7f09de1-35dd-4281-975f-7d91e41a5e0a",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png"
+                },
+                {
+                    "id": "4d38f813-2553-423f-af51-95908cb4fa1f",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/120.png"
+                },
+                {
+                    "id": "d4cde3fd-3e21-4490-b7d4-7f4d05e4c4a5",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/103.png"
+                },
+                {
+                    "id": "242cc837-b415-402e-b395-7eaee8e50ac1",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                }
+            ]
+        },
+        {
+            "id": "ae9f36c4-4185-4793-a26a-2f8cd5a55184",
+            "club_title": "everybody",
+            "club_duration": "member",
+            "club_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/164.png",
+            "image_list": [
+                {
+                    "id": "4b764866-a8a7-4a87-af81-9d56738a1629",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/192.png"
+                },
+                {
+                    "id": "2692635d-ab6c-452e-833c-415541151551",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                },
+                {
+                    "id": "959786fc-b679-4a12-bde7-80b200288321",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                },
+                {
+                    "id": "21ea149c-770a-45f8-bdf3-4aadab0758e5",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png"
+                },
+                {
+                    "id": "9f3dd98a-4536-4c3f-a9e7-a642500b0264",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/304.png"
+                }
+            ]
+        },
+        {
+            "id": "10483538-9b92-4647-8d00-d10c7b31dd95",
+            "club_title": "life",
+            "club_duration": "song",
+            "club_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png",
+            "image_list": [
+                {
+                    "id": "dc9f0ac0-7d05-41e1-bcd0-d7cb8c85e806",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                },
+                {
+                    "id": "cac13e30-a529-499e-bb01-3887dfa9a983",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/358.png"
+                },
+                {
+                    "id": "71345651-79d9-4acf-9e9d-3bbc1aa548c3",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "552641ae-3945-4b72-ad65-553ece6efd0f",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png"
+                },
+                {
+                    "id": "547fdbe0-9014-44dd-82bb-2e781cb4b2a5",
+                    "icons": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                }
+            ]
+        }
+    ]
+}
